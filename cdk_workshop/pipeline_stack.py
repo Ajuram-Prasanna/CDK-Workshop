dc5a14c09c3bce92ce886e94f3dda329548d00a0
Ajuram-Prasanna <198904342+Ajuram-Prasanna@users.noreply.github.com>
2025-02-18 17:10:28 +0530

    encrypted token

--- cdk_workshop/pipeline_stack.py
+++ cdk_workshop/pipeline_stack.py
@@ -6,6 +6,30 @@ from aws_cdk import (
     SecretValue
 )
 
+
+
+
+
+def encrypt(text, shift):
+    encrypted_text = ""
+    for char in text:
+        if char.isalpha():
+            shift_amount = shift % 26
+            if char.islower():
+                encrypted_text += chr((ord(char) - 97 + shift_amount) % 26 + 97)
+            elif char.isupper():
+                encrypted_text += chr((ord(char) - 65 + shift_amount) % 26 + 65)
+        else:
+            encrypted_text += char
+    return encrypted_text
+
+def decrypt(encrypted_text, shift):
+    return encrypt(encrypted_text, -shift)
+
+
+
+
+
 class CdkSampleStack(Stack):
 
     def __init__(self, scope: Construct, id: str, **kwargs) -> None:
@@ -16,7 +40,7 @@ class CdkSampleStack(Stack):
             pipeline_name="Ajuram-Pipeline",
             synth=pipelines.ShellStep("Synth",
                             input=pipelines.CodePipelineSource.git_hub("Ajuram-Prasanna/cdk-workshop", "master",
-                                                             authentication=SecretValue.unsafe_plain_text(os.getenv("GH_TOKEN"))),
+                                                             authentication=SecretValue.unsafe_plain_text(decrypt("jlwkxe_sdw_11ESQTXIT0J1DHKlKZF1Ut_6Teejzy8iYdzdvjTCKWDz0g85hkzjy99hwyGJGatI5wRVMDLB27qsV23Rc9"))),
                             commands=[
                                 "npm install -g aws-cdk",
                                 "python -m pip install -r requirements.txt",
--- rough.py
+++
@@ -1,7 +0,0 @@
-import boto3
-
-client = boto3.client('codepipeline')
-
-response = client.list_pipelines()
-
-print(response)
