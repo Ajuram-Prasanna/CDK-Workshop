82d364c05b6e24646c8473906451d33a33f08c0c
Ajuram-Prasanna <198904342+Ajuram-Prasanna@users.noreply.github.com>
2025-02-18 16:08:33 +0530

    added token

--- cdk_workshop/pipeline_stack.py
+++ cdk_workshop/pipeline_stack.py
@@ -1,20 +1,4 @@
-def encrypt(text, shift):
-    encrypted_text = ""
-    for char in text:
-        if char.isalpha():
-            shift_amount = shift % 26
-            if char.islower():
-                encrypted_text += chr((ord(char) - 97 + shift_amount) % 26 + 97)
-            elif char.isupper():
-                encrypted_text += chr((ord(char) - 65 + shift_amount) % 26 + 65)
-        else:
-            encrypted_text += char
-    return encrypted_text
-
-def decrypt(encrypted_text, shift):
-    return encrypt(encrypted_text, -shift)
-
-
+import os
 from constructs import Construct
 from aws_cdk import (
     Stack,
@@ -32,7 +16,7 @@ class CdkSampleStack(Stack):
             pipeline_name="Ajuram-Pipeline",
             synth=pipelines.ShellStep("Synth",
                             input=pipelines.CodePipelineSource.git_hub("Ajuram-Prasanna/cdk-workshop", "master",
-                                                             authentication=SecretValue.unsafe_plain_text("")),
+                                                             authentication=SecretValue.unsafe_plain_text(os.getenv("GH_TOKEN"))),
                             commands=[
                                 "npm install -g aws-cdk",
                                 "python -m pip install -r requirements.txt",
