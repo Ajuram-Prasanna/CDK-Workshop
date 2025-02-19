91a61aeab4d07be23d167255679143a7a738f6e1
Ajuram-Prasanna <198904342+Ajuram-Prasanna@users.noreply.github.com>
2025-02-19 14:38:37 +0530

    removed lambda

--- cdk_workshop/cdk_sample_stack.py
+++ cdk_workshop/cdk_sample_stack.py
@@ -8,19 +8,3 @@ from aws_cdk import (
 
 def __init__(self, scope: Construct, id: str, **kwargs):
     super().__init__(scope, id, **kwargs)
-
-    # Defines an AWS Lambda resource
-    my_lambda = _lambda.Function(
-        self,
-        "HelloHandler",
-        function_name="cdk-lambda",
-        runtime=_lambda.Runtime.PYTHON_3_7,
-        code=_lambda.Code.from_asset("lambda"),
-        handler="hello.handler",
-    )
-
-    gateway = apigw.LambdaRestApi(
-        self,
-        "Endpoint",
-        handler=my_lambda,
-    )
